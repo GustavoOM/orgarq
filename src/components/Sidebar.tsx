dd56dab5a11f55d1cd596b9fa324011100f5d8b3
GustavoOM <gumartins2001@gmail.com>
2022-07-28 19:16:01 -0300

    ajuste nos estilos

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,7 +17,7 @@ export function Sidebar(){
             </div>
             <div className={styles.email}>
                 <Envelope size={20}/>
-                sarita@icmc.usp.br
+                <a href="mailto:sarita@icmc.usp.br?subject=D%C3%BAvida%20ou%20sugest%C3%A3o%20do%20site%20de%20recursos%20educacionais&body=Ol%C3%A1%20Sarita%2C%0D%0A%0D%0AVim%20pelo%20site%20de%20recursos%20educacionais%20e%20gostaria%20de...">sarita@icmc.usp.br</a>
 
             </div> 
             <footer>
